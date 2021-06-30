c7f13666ca3a71029de32ce9a77284f33d77073e
Jefte Lopes <jeftelg72@gmail.com>
2021-06-30 14:06:15 -0300

    retirada de seção da home

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -173,37 +173,6 @@
             <main class="mt-5">
                 <div class="container">
                     <!--Section: Content-->
-                    <section>
-                        <div class="row">
-                            <div class="col-md-6 gx-5 mb-4">
-                                <div class="bg-image hover-overlay ripple shadow-2-strong" data-mdb-ripple-color="light">
-                                    <img src="https://www.moblee.com.br/blog/wp-content/uploads/sites/2/2019/04/porque-acompanhar-uma-palestra-de-encantamento-em-eventos1.png"
-                                        class="img-fluid" />
-                                    <a href="#!">
-                                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
-                                    </a>
-                                </div>
-                            </div>
-
-                            <div class="col-md-6 gx-5 mb-4">
-                                <h4><strong>Facilis consequatur eligendi</strong></h4>
-                                <p class="text-muted">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis consequatur
-                                    eligendi quisquam doloremque vero ex debitis veritatis placeat unde animi laborum
-                                    sapiente illo possimus, commodi dignissimos obcaecati illum maiores corporis.
-                                </p>
-                                <p><strong>Doloremque vero ex debitis veritatis?</strong></p>
-                                <p class="text-muted">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod itaque voluptate
-                                    nesciunt laborum incidunt. Officia, quam consectetur. Earum eligendi aliquam illum
-                                    alias, unde optio accusantium soluta, iusto molestiae adipisci et?
-                                </p>
-                            </div>
-                        </div>
-                    </section>
-                    <!--Section: Content-->
-
-                    <hr class="my-5" />
 
                     {{-- <!--Section: Content-->
                     <section class="text-center" id=palestras>
